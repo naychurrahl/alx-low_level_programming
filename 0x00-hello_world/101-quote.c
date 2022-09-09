5974f6e6e07c581c954d836a512f916a38353b4f
naychurrahl <onifadej@gmail.com>
2022-09-08 20:22:10 -0700

    wo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	int text = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", text;
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", text;);
 	return (0);
 }
