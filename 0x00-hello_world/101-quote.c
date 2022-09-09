e01b12d39930c15e3b82ced08c77513dfe08c2ae
naychurrahl <onifadej@gmail.com>
2022-09-08 20:39:00 -0700

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fprintf(stderr , text);
+(
+	fprintf(stderr , "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
