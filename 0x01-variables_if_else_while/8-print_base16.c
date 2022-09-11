89201b13f81da5f52ce61ceee9a66ecf5454be2b
naychurrahl <onifadej@gmail.com>
2022-09-11 09:24:33 -0700

    hold up

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	for (i = 'a'; i <= 'e'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
