564a5c133d7836c05be25ccad4584a16c6fe5d25
naychurrahl <onifadej@gmail.com>
2022-09-11 11:22:55 -0700

    char

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			if (j = i)
+			{
+				continue;
+			} else if (j != 1)
+			{
+				putchar(' ');
+			}
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if ((i <= 8) & (j != 9))
+			{
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
