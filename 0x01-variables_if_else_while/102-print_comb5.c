c68bce20216644a0d597ae9b74df64308515b740
naychurrahl <onifadej@gmail.com>
2022-09-12 22:03:02 -0700

    Paranoid

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,35 +2,34 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success/correct)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100 ; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = j + 1 ; j < 100; j++)
+		for (j = i; j < 100; j++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar (' ');
-			putchar (j / 10 + '0');
-			putchar (j % 10 + '0');
-
-			if (i == 98 && j == 99)
+			if (j == i)
 			{
-				break;
+				continue;
+			} else if (j != 1)
+			{
+				putchar(' ');
 			}
-
-			putchar(',');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98)
+			{
+				putchar(',');
+			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
---
+++ 0x01-variables_if_else_while/102-print_comb5.c.bak
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100 ; i++)
+	{
+		for (j = j + 1 ; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar (' ');
+			putchar (j / 10 + '0');
+			putchar (j % 10 + '0');
+
+			if (i == 98 && j == 99)
+			{
+				break;
+			}
+
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
