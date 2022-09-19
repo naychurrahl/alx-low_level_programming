017715ae8fc019b52495dbca1883113b47c97cc5
naychurrahl <onifadej@gmail.com>
2022-09-19 09:22:57 -0700

    I stand corrected

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * reset to just before '99
+ * reset_to_98 - resets to just before '99
  * Yeah I know it sucks no party
- * 
+ * @m: pointer
  * Return: (0)
  */
 void reset_to_98(int *m)
 {
 	*m = 98;
-	_putchar('\n');
 }
