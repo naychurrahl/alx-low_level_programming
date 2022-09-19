709404f0a34f29629da9eb547eee6a25f2fcb43a
naychurrahl <onifadej@gmail.com>
2022-09-19 09:12:14 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset to just before '99
+ * Yeah I know it sucks no party
+ * 
+ * Return: (0)
+ */
+void reset_to_98(int *m)
+{
+	*m = 98;
+	_putchar('\n');
+	return(0);
+}
