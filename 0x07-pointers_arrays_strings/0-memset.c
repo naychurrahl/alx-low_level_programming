1c42b3621a7506265489d1021e015f51e8543609
naychurrahl <onifadej@gmail.com>
2022-09-26 16:07:28 -0700

    suicide

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,17 @@
-#include <main.h>
-
 /**
+ * _strcat - function that joins two strings.
  *
+ * @s: pointer to destnation input
+ * @b: pointer to source input
+ * @n: some other important thing
  *
+ * Return: s
  */
 
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	for(i = 0; i < n; i++)
+		s[i] = b;
+	return(s);
+}
